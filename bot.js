26063d874b400fc6c447f7fcdbc47fd053692fc8
Galactic Chicken <52435610+Tio-Pollo@users.noreply.github.com>
2019-07-02 12:15:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,14 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
- 
-    if (message.content == 'ping') {
+
+    let m;
+
+    if ((m = /^!ratio(?: +(\S.*))?/i.exec(message.content)) !== null) {
+        if (m[1]) {
+            message.channel.send('https://jeroenr.nl/gf-dt/ratio.png?q=' + encodeURIComponent(m[1]));
+        }
+    } else if (message.content == 'ping') {
         //message.reply('pong');  message.channel.send()
         message.channel.send('pong');
     } else if (message.content == 'say hi') {
