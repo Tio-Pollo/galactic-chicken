11896ff3d7ea9bcc8578cc7fed3136c5b1e8421d
Galactic Chicken <52435610+Tio-Pollo@users.noreply.github.com>
2019-07-03 00:40:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,39 +2,45 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Cluck cluck! 🐔');
 });
+
+const re = {
+    ratio: /^!ratio(?: +(\S.*))?/i,
+    sendmsg: /^!sendmsg +(\S+) (.+)/i,
+    chicken: /\bchicken\b/i,
+    headoff: /^off with his head/i,
+    coffee: /^\W*coffee\W*(?:please\W*)?$/i
+};
 
 client.on('message', message => {
 
-    let m;
+    let m, nick, msg;
+    msg = message.content;
 
-    if ((m = /^!ratio(?: +(\S.*))?/i.exec(message.content)) !== null) {
+    //message.reply('pong');  message.channel.send()
+    
+    if ((m = re.ratio.exec(msg)) !== null) {
         const ratioURL = process.env.JEROENR_RATIO;
-        message.channel.send(ratioURL + '?q=' + encodeURIComponent(m[1] ? m[1] : message.author.tag.split('#')[0]));
-        //message.reply('pong');  message.channel.send()
-    } else if (message.content == 'ping') {
+        nick = (message.guild.member(message.author).nickname || message.author.tag.split('#')[0]);
+        message.channel.send(ratioURL + '?q=' + encodeURIComponent(m[1] ? m[1] : nick));
+    } else if (msg.toLowerCase() == 'ping') {
         message.channel.send('pong');
-    } else if (message.content == 'nicktest') {
-        //let nick = ('' + message.member).split('#')[0].replace(/\W+/g,'');
-        let nick = 'nope';
-        try {
-            nick = message.guild.member(message.author).nickname;
-        } catch (e) {
-        }
+    } else if (msg.toLowerCase() == 'nicktest') {
+        nick = (message.guild.member(message.author).nickname || message.author.tag.split('#')[0]);
         message.channel.send('Hello ... ' + nick);
-    } else if ((m = /^!sendmsg +(\S+) (.+)/i.exec(message.content)) !== null) {
+    } else if ((m = re.sendmsg.exec(msg)) !== null) {
         const channel = client.channels.find('name', m[1]);
         if (channel) {
             channel.send(m[2]);
         }
-    } else if (message.content == 'say hi') {
+    } else if (msg.toLowerCase() == 'say hi') {
         message.channel.send('Hi!');
-    } else if (/\bchicken\b/i.test(message.content)) {
+    } else if (re.chicken.test(msg)) {
         message.react('🐔');
-    } else if (/^off with his head/i.test(message.content)) {
+    } else if (re.headoff.test(msg)) {
         message.channel.send("I'm hidding behind Fireball!");
-    } else if (/^\W*coffee$/i.test(message.content)) {
+    } else if (re.coffee.test(message.content)) {
         message.channel.send('☕');
     }
 });
